262b2f0045d5eff0e463066cc56ddacab1faef2d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-03-14 02:38:49 -0400

    Add ExternalProject_SetIfNotDefined macro

--- ExternalProjectDependency.cmake
+++ ExternalProjectDependency.cmake
@@ -787,3 +787,46 @@ function(ExternalProject_Install_CMake project_name)
 
   install(SCRIPT ${binary_dir}/cmake_install.cmake)
 endfunction()
+
+#.rst:
+# .. cmake:function:: ExternalProject_SetIfNotDefined
+#
+# Set a variable to its default value if not already defined.
+#
+# .. code-block:: cmake
+#
+#  ExternalProject_SetIfNotDefined(<var> <defaultvalue> [OBFUSCATE])
+#
+# The default value is set with:
+#  (1) if set, the value environment variable <var>.
+#  (2) if set, the value of local variable variable <var>.
+#  (3) if none of the above, the value passed as a parameter.
+#
+# Setting the optional parameter 'OBFUSCATE' will display 'OBFUSCATED' instead of the real value.
+macro(ExternalProject_SetIfNotDefined var defaultvalue)
+  set(_obfuscate FALSE)
+  foreach(arg ${ARGN})
+    if(arg STREQUAL "OBFUSCATE")
+      set(_obfuscate TRUE)
+    endif()
+  endforeach()
+  if(DEFINED ENV{${var}} AND NOT DEFINED ${var})
+    set(_value "$ENV{${var}}")
+    if(_obfuscate)
+      set(_value "OBFUSCATED")
+    endif()
+    message(STATUS "Setting '${var}' variable with environment variable value '${_value}'")
+    set(${var} $ENV{${var}})
+  endif()
+  if(NOT DEFINED ${var})
+    set(_value "${defaultvalue}")
+    if(_obfuscate)
+      set(_value "OBFUSCATED")
+    endif()
+    message(STATUS "Setting '${var}' variable with default value '${_value}'")
+    set(${var} "${defaultvalue}")
+  endif()
+  if(NOT _obfuscate)
+    list(APPEND variables ${var})
+  endif()
+endmacro()
